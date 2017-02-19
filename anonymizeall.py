4966826d60a6c15a68d2109b232d7de8099c18a2
marioBZ7 <mario.briseno7@gmail.com>
2017-02-19 15:09:58 -0600

    Create anonymizeall.py

---
+++ anonymizeall.py
@@ -0,0 +1,25 @@
+__author__ = 'Mario'
+
+import os
+from pydicom.examples.anonymize import anonymize
+
+list= os.listdir()
+
+idx=[] #    Empty list
+
+#To get index of non dicom files
+for x in list:
+    hh= list.index(x)
+    if x[len(x)-3:len(x)] == '.py':
+        idx.append(hh)
+    if x == '.idea':
+        idx.append(hh)
+
+#Erase non-dicom files
+for index in sorted(idx, reverse= True):
+    del list[index]
+
+for x in list:
+    anonymize(x, 'anon'+x)
+
+print("All dicom files in this folder have been anonymized")
