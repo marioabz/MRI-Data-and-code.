338c15405f6a3ec848f968e79d7fecc7c63211bf
marioBZ7 <mario.briseno7@gmail.com>
2017-03-01 21:54:46 -0600

    Update unfoldfiles.py

--- unfoldfiles.py
+++ unfoldfiles.py
@@ -1,32 +1,35 @@
 __author__ = 'Mario'
 import os
 import numpy, pylab, pydicom
-list=os.listdir()
 
-# filename=os.path.basename(sys.argv[0])    
-#list=['4589','.idea','jkj.py', 'ghf.py', '5412'] Testing
+def callImages():
 
-idx=[]
+    list=os.listdir()
+  
+    #list=['4589','.idea','jkj.py', 'ghf.py', '5412'] Testing
 
-for x in list:
-    hh= list.index(x)
+    idx=[]
+    for x in list:
+        hh= list.index(x)
+
+        if x[len(x)-3:len(x)]=='.py' |  x=='.idea':
+            idx.append(hh)
+
+
+    for index in sorted(idx, reverse=True):
+        del list[index]
+
+    #Make a big data tuple to handle in one variable all images.
+    #Implement for loop for that
+
+    X=[]
+    for inx in list:
+        X.append(pydicom.read_file(inx))
     
-    if x[len(x)-3:len(x)]=='.py' |  x=='.idea':
-        idx.append(hh)
+    return X
 
- 
-for index in sorted(idx, reverse=True):
-    del list[index]
-
-#Make a big data tuple to handle in one variable all images.
-#Implement for loop for that
-
-X=[]
-for inx in list:
-    X.append(pydicom.read_file(inx))
-
-"For displaying all images"
-"""for xy in X:
-    pylab.imshow(xy.pixel_array, cmap=pylab.cm.gray)
-    pylab.show()
-    input('Next...')"""
+    "For displaying all images"
+    """for xy in X:
+        pylab.imshow(xy.pixel_array, cmap=pylab.cm.gray)
+        pylab.show()
+        input('Next...')"""
