ea2247653990a89a655c531e8c85e4cb0ca1bccb
marioBZ7 <mario.briseno7@gmail.com>
2017-03-04 23:59:01 -0600

    Create norm.py

---
+++ norm.py
@@ -0,0 +1,20 @@
+__author__ = 'Mario'
+
+from sklearn import preprocessing
+import pylab
+from hist import hist
+from Unfoldfiles import callImages
+
+X= callImages()
+
+for x in X
+  x,y=x.shape
+
+
+pylab.plot(x)
+pylab.plot(y)
+pylab.show()
+
+#This allows to watch the range of gray intensities to see if normalization could work.
+#Pre-processing step
+
