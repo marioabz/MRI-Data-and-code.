220f053979648b0ada4cc2a4723c3e9c36e2b2af
marioBZ7 <mario.briseno7@gmail.com>
2017-03-12 18:05:52 -0600

    Update cropping.py

--- cropping.py
+++ cropping.py
@@ -1,8 +1,8 @@
 @author: Mario
 
-from matplotlib.pyplot import draw,ion
+
 import matplotlib.pyplot as plt
-import dicom
+import dicom, cv2
 import numpy as np
 import tkinter as tk
 from tkinter import filedialog
@@ -14,33 +14,44 @@ def scale(X):
     A=(X-mn)/(mx-mn)
     return np.float32(A)
 
-axes = np.zeros((2, 2), dtype=np.object)
-root = tk.Tk()
-root.withdraw()
-folder= filedialog.askopenfilenames()
-
+cropping = False
+axes = np.zeros((1, 3), dtype=np.object)
+folder= filedialog.askopenfilename()
 ls=[]
-for x in folder:
-    ls.append(dicom.read_file(x).pixel_array)
 
-ion()
-fig = plt.figure()
-axes[0,0]=fig.add_subplot(1,2,1)
-axes[1,0]=fig.add_subplot(1,2,2)
-axes[0,0].imshow(ls[0],cmap='gray')
-axes[1,0].imshow(ls[1],cmap='gray')
+def ccrop(event, x, y, flags, param):
+	global coords, cropping   
+	if event == cv2.EVENT_LBUTTONDOWN:
+		coords = [(x, y)]
+		cropping = True
+	elif event == cv2.EVENT_LBUTTONUP:
+		coords.append((x, y))
+		cropping = False 
+		cv2.rectangle(image, coords[0], coords[1], (0, 0, 255), 2)
+        cv2.imshow("Patient", image)
 
+image3 = scale(dicom.read_file(folder).pixel_array)
+image=np.zeros(image3.shape+(3,),image3.dtype)
 
-
-input('Click enter...')
-
-for i in range(len(folder)):
-    x0=int(input('Punto x de primera esquina: '))
-    y0=int(input('Punto y de primera esquina: '))
-    xf=int(input('Punto x de contra esquina: '))
-    yf=int(input('Punto y de contra esquina: '))
+for z in range(3):
+    image[:,:,z]=image3
     
-    if x0 > xf:
+clone = image3.copy()
+cv2.namedWindow("Patient")
+cv2.setMouseCallback("Patient", ccrop)
+
+while True:
+	cv2.imshow("Patient", image)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("r"):
+		image = clone.copy()
+	elif key == ord("c"):
+		break
+        
+coords[0]=[x0,y0]
+coords[1]=[xf,yf]
+
+if x0 > xf:
         x0,xf = xf,x0
     elif x0 == xf:
         x0, xf= -1
@@ -49,21 +60,27 @@ for i in range(len(folder)):
         y0,yf = yf,y0
     elif y0 == yf:
         y0, yf= -1
-    
-    sub_im= ls[i][x0:xf,y0:yf] ## Windows
-    corn0=np.arange(x0,xf+1)
-    corn1=np.arange(y0,yf+1)
-    sc_img = scale(ls[i]) ## Scaled and casted
-    z_arr = np.zeros(ls[i].shape+(3,), sc_img.dtype)
-    for t in range(3):
-        z_arr[:,:,t]=sc_img  ##Building 3D array
-    z_arr[x0,corn1,0],z_arr[xf,corn1,0]=1,1
-    z_arr[y0,corn0,0],z_arr[yf,corn0,0]=1,1
-    z_arr[x0,corn1,1],z_arr[xf,corn1,1]=0,0
-    z_arr[y0,corn0,1],z_arr[yf,corn0,1]=0,0    
-    z_arr[x0,corn1,2],z_arr[xf,corn1,2]=0,0
-    z_arr[y0,corn0,2],z_arr[yf,corn0,2]=0,0
-    axes[i,1]=fig.add_subplot(2,2,2+i*2)
+        
+if len(coords) == 2:
+    	roi = clone[x0:xf, y0:yf]
+    	cv2.imshow("Region of Interest", roi)
+    	cv2.waitKey(0)
 
-plt.show(block=False)
-    
+cv2.destroyAllWindows()
+
+fig=plt.figure(figsize=(12,6))
+d=np.where(image[:,:,2] == 255)
+image[d[0],d[1],2]=1
+
+fig=plt.figure(figsize=(12,6))
+im=axes[0,0]=fig.add_subplot(1,3,1)
+roiim=axes[0,1]=fig.add_subplot(1,3,2)
+im.set_axis_off()
+im.set_title('Imagen original',fontsize=10)
+roiim.set_title('Regíon de interés',fontsize=10)
+roiim.set_axis_off()
+axes[0,0].imshow(image)
+axes[0,1].imshow(roi,cmap='gray')
+f=axes[0,2]=fig.add_subplot(1,3,3)
+axes[0,2].hist(roi.ravel(),bins=150,color='blue')
+f.set_title('Histograma de región de interés',fontsize=10)
