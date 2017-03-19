a3a1429b5f4b544bb5cfc190e006a43604628c87
marioBZ7 <mario.briseno7@gmail.com>
2017-03-19 04:11:23 -0600

    Updating and editing cropping

--- cropping.py
+++ cropping.py
@@ -1,11 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 13:06:07 2017
+
 @author: Mario
+"""
 
-
-import matplotlib.pyplot as plt
-import dicom, cv2
+import cv2, dicom
 import numpy as np
-import tkinter as tk
+
 from tkinter import filedialog
+import matplotlib.pyplot as plt
 
 def scale(X):
     A=np.array(X)
@@ -14,11 +18,10 @@ def scale(X):
     A=(X-mn)/(mx-mn)
     return np.float32(A)
 
+coords = []
 cropping = False
-axes = np.zeros((1, 3), dtype=np.object)
 folder= filedialog.askopenfilename()
-ls=[]
-
+    
 def ccrop(event, x, y, flags, param):
 	global coords, cropping   
 	if event == cv2.EVENT_LBUTTONDOWN:
@@ -28,15 +31,15 @@ def ccrop(event, x, y, flags, param):
 		coords.append((x, y))
 		cropping = False 
 		cv2.rectangle(image, coords[0], coords[1], (0, 0, 255), 2)
-        cv2.imshow("Patient", image)
-
+		cv2.imshow("Patient", image)
+        
 image3 = scale(dicom.read_file(folder).pixel_array)
 image=np.zeros(image3.shape+(3,),image3.dtype)
 
 for z in range(3):
     image[:,:,z]=image3
     
-clone = image3.copy()
+clone = image.copy()
 cv2.namedWindow("Patient")
 cv2.setMouseCallback("Patient", ccrop)
 
@@ -47,28 +50,28 @@ while True:
 		image = clone.copy()
 	elif key == ord("c"):
 		break
-        
-coords[0]=[x0,y0]
-coords[1]=[xf,yf]
+    
+[x0,y0]=coords[0]
+[xf,yf]=coords[1]
 
 if x0 > xf:
-        x0,xf = xf,x0
-    elif x0 == xf:
-        x0, xf= -1
-        
-    if y0 > yf:
-        y0,yf = yf,y0
-    elif y0 == yf:
-        y0, yf= -1
-        
+    x0,xf = xf,x0
+elif x0 == xf:
+    x0, xf= -1
+if y0 > yf:
+    y0,yf = yf,y0
+elif y0 == yf:
+    y0, yf= -1
+ 
 if len(coords) == 2:
-    	roi = clone[x0:xf, y0:yf]
+    	roi = clone[y0:yf,x0:xf]
     	cv2.imshow("Region of Interest", roi)
     	cv2.waitKey(0)
+    
 
 cv2.destroyAllWindows()
+axes = np.zeros((1, 3), dtype=np.object)
 
-fig=plt.figure(figsize=(12,6))
 d=np.where(image[:,:,2] == 255)
 image[d[0],d[1],2]=1
 
@@ -84,3 +87,5 @@ axes[0,1].imshow(roi,cmap='gray')
 f=axes[0,2]=fig.add_subplot(1,3,3)
 axes[0,2].hist(roi.ravel(),bins=150,color='blue')
 f.set_title('Histograma de región de interés',fontsize=10)
+
+
