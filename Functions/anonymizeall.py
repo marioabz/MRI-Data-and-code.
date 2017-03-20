cd5efc45328a92e3ad8b4b3c22d2704af413a650
marioBZ7 <mario.briseno7@gmail.com>
2017-03-19 19:32:44 -0600

    Hiding location of MRI study

--- Functions/anonymizeall.py
+++ Functions/anonymizeall.py
@@ -2,15 +2,15 @@ __author__ = 'Mario'
 
 import os, dicom
 from dicom.examples.anonymize import anonymize
+from dicom.UID import generate_uid
 
 def anonymize_all(folder):
-    
+
     list= os.listdir(folder) #Folders within gral. folder.
-    idx=[] #    Empty list
     anonfolder = folder+'ANMZ'
-    os.makedir(anonfolder)
+    os.mkdir(anonfolder)
     #To get index of non dicom files
-    
+        
     for z in list:  #List of subfolders
         temp_var=folder+'/'+z
         for w in os.listdir(temp_var):
@@ -19,15 +19,16 @@ def anonymize_all(folder):
             os.remove(an_var)
             data.file_meta.MediaStorageSOPClassUID = '...'
             data.file_meta.MediaStorageSOPInstanceUID = generate_uid()
+            data.InstitutionName='No location'
             data.save_as(an_var)
-            
+                
     #Adding necesary tags to properly anonymize all dicom files.
-    
+        
     for x in list:
         tempdir=anonfolder+'/'+x+'ANMZ'
         os.makedirs(tempdir)
         tempfold=folder+'/'+x
         for y in os.listdir(tempfold):
             anonymize(tempfold+'/'+y, tempdir+'/'+'anon'+y)
-            
+                
     print("All dicom files in this folder have been anonymized")
