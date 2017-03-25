30940205ebd6da946f00e03c31ec1fc8cbf9242e
marioBZ7 <mario.briseno7@gmail.com>
2017-03-25 14:40:35 -0600

    Creating new class

---
+++ Preprocessing.py
@@ -0,0 +1,23 @@
+
+"""
+@author: Mario
+"""
+
+from scipy.stats import signaltonoise as s2n
+from skimage import exposure
+import numpy as np
+
+
+
+class Preprocessing(object):
+    def __init__(self):
+        
+        self.s2nR= 0.0
+    
+    def ContrastEnhancement(self):
+        
+        
+        
+        
+        
+        
