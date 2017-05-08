a24e88e6aa26b648fcd4b0bad06d8e88018ec302
marioBZ7 <mario.briseno7@gmail.com>
2017-05-08 16:39:11 -0500

    Adding methods

--- Preprocessing.py
+++ Preprocessing.py
@@ -24,24 +24,24 @@
 
 
 from scipy.stats import signaltonoise as s2nR
-from skimage import exposure, rank
+from skimage import exposure
 from numpy import zeros, mean
 from tkinter import Tk
 from Functions.sel_range import get_range
 from Functions.mask import select_threshold_mask
 from skfuzzy import defocus_local_means
 from skfuzzy.image import nmse
-from skimage.filters import sobel
-
-
+from skimage.filters import sobel,median
+from skimage.filters.rank import enhance_contrast
+from skimage.morphology import disk,square
+from skimage.util import random_noise
 
 class Preprocessing(object):
     
     def __init__(self, x):
         self.s2nR=         0.0
-        self.Img =         x
-        self.img_stretch=  zeros(x.shape, float)
-        self.defocus_img=  zeros(x.shape, float)
+        self.img =         x
+        self.img_over=     x
     
     def contrast_stretch(self):
         master= Tk();
@@ -51,27 +51,35 @@ class Preprocessing(object):
     def s2nR(self):
         self.s2nR=s2nR(self.Img, axis=None) 
         
-    def histogram_eq(self, S, _mask_sltr=True):
+    def histogram_eq(self, _mask_sltr=True):
         if _mask_sltr == True:
-            thrld= select_threshold_mask(S)
+            thrld= select_threshold_mask(self.img)
         else:
-            thrld= mean(S)
-        return exposure.equalize_hist(S,mask=S>thrld)
+            thrld= mean(self.img)
+        self.img_over= exposure.equalize_hist(self.img ,mask=self.img>thrld)
     
-    def adaptive_hist(self,img):
-        return exposure.equalize_adapthist(img)
+    def adaptive_hist(self,kernel=3):
+        self.img_over= exposure.equalize_adapthist(self.img,kernel_size=kernel)
     
     def nmse(self,x,y):
         return nmse(x,y)
     
     def dlm(self):
-        return defocus_local_means(self.Img)
+        self.img_over= defocus_local_means(self.Img)
     
-    def contrast_enhancement(self):
-        return 
+    def contrast_enhancement(self, local_mtx=disk(5)):
+        self.img_over= enhance_contrast(self.Img, local_mtx)
     
     def detect_borders(self):
-        
+        return sobel(self.Img)
+    
+    def add_sp_noise(self,percentage=0.5):
+        self.img_over= random_noise(self.Img, mode='s&p', salt_vs_pepper=percentage)
+    
+    def median_filter(self,struct=disk(5)):
+        self.img_over= median(self.Img, selem=struct)
+    
+    
     
     
         
