f4763983943fe97819b604aa71c23b26fdbd5d36
marioBZ7 <mario.briseno7@gmail.com>
2017-05-15 23:12:26 -0500

    Adding license messages

--- Functions/anonymizeall.py
+++ Functions/anonymizeall.py
@@ -1,5 +1,23 @@
 __author__ = 'Mario'
 
+########################################################################
+#   Anonymize function: Function to anonymize dicom files              #
+#                                                                      #
+#                                                                      #
+# This program is free software: you can redistribute it and/or modify #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# at your option) any later version.                                   #
+#                                                                      #
+# This program is distributed in the hope that it will be useful,      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    # 
+# along with this program. If not, see <http://www.gnu.org/licenses/>. #
+########################################################################
+
 import os, dicom
 from dicom.examples.anonymize import anonymize
 from dicom.UID import generate_uid
