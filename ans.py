0f678c8f6c49934d28dcb2a715256207e7edfe3f
marioBZ7 <mario.briseno7@gmail.com>
2017-05-24 22:42:19 -0500

    Expert answers on segmented images

---
+++ ans.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 20:31:01 2017
+
+@author: Mario
+"""
+
+'Expert answer'
+
+['Si',
+ 'si',
+ 'p',
+ 'no',
+ 'no',
+ 'no',
+ 'p',
+ 'p',
+ 'p',
+ 'p',
+ 'no',
+ 'no',
+ 'p',
+ 'p',
+ 'p',
+ 'si',
+ 'no',
+ 'p',
+ 'p',
+ 'no',
+ 'no',
+ 'p',
+ 'si',
+ 'no',
+ 'no',
+ 'si',
+ 'si',
+ 'no',
+ 'no',
+ 'p',
+ 'no',
+ 'si',
+ 'p']
