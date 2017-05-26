95cfe658c7b5aa08c96f50f58472d971974e2613
marioBZ7 <mario.briseno7@gmail.com>
2017-05-25 23:29:30 -0500

    Print expert answers in latex format

--- ans.py
+++ ans.py
@@ -4,10 +4,12 @@ Created on Wed May 24 20:31:01 2017
 
 @author: Mario
 """
+from gtRelevantData import gtRelevantData
+from os import listdir
+import numpy as np,dicom
 
-'Expert answer'
-
-['Si',
+'Expert answers'
+ans=['si',      
  'si',
  'p',
  'no',
@@ -37,6 +39,41 @@ Created on Wed May 24 20:31:01 2017
  'no',
  'no',
  'p',
- 'no',
  'si',
  'p']
+clus=[4, 4, 4, 3, 3, 3, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 4, 4, 4, 3, 3, 4, 4, 4, 4, 4, 4, 3, 3]
+print(' & T1 & T2 ')
+for i in ans:
+    if i=='si':
+        ans[ans.index(i)]='\\ding{51}'
+    elif i=='no':
+        ans[ans.index(i)]='\\ding{55}'
+    else:
+        ans[ans.index(i)]='--       '
+ans=np.array(ans).reshape((16,2))
+cont=[]
+
+for i in listdir('DCMfiles'):
+    for j in listdir('DCMfiles'+'/'+i):
+        cont.append(gtRelevantData(dicom.read_file('DCMfiles'+'/'+i+'/'+j)).AcquisitionContrast)
+
+for x,i in enumerate(cont):
+    cont[x]='T1' in i
+
+cont=np.array(cont).reshape((16,2))
+clus=np.array(clus).reshape((16,2))
+
+for x,[i,j] in enumerate(cont):
+    if j:
+        ans[x,0],ans[x,1]=ans[x,1],ans[x,0]
+
+
+for s in range(16):
+    print(s,'&  ', clus[s,0], '&   ', ans[s,0],'&   ',clus[s,0], '&' ,ans[s,1])
+    
+    
+    
+        
+        
+        
+    
